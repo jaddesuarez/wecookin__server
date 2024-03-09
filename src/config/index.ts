e8a912880109a27e28c4c26eb3e2732397984d84
Jadde Suarez <jaddesuarez@gmail.com>
2024-03-09 13:26:29 +0100

    update front url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,22 +1,22 @@
-import express, { type Express } from 'express'
-import logger from 'morgan'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express, { type Express } from "express";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const FRONTEND_URL = process.env.ORIGIN ?? 'http://localhost:3000'
+const FRONTEND_URL = process.env.ORIGIN ?? "http://localhost:3000";
 
 export default (app: Express): void => {
-  app.set('trust proxy', 1)
+  app.set("trust proxy", 1);
 
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: [FRONTEND_URL, "https://wecookin.vercel.app/"],
     })
-  )
+  );
 
-  app.use(logger('dev'))
+  app.use(logger("dev"));
 
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: false }))
-  app.use(cookieParser())
-}
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+};
